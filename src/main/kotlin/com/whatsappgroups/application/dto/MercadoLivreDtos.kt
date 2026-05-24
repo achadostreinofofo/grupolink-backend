753dafa401bc0605c277ffb67c97f18cb8b2cd16
achadostreinofofo <achadostreinofofo@gmail.com>
2026-05-24 19:18:20 -0300

    feat: add MlAffiliateParamsRequest, affiliateConfigured to MlStatusResponse

--- src/main/kotlin/com/whatsappgroups/application/dto/MercadoLivreDtos.kt
+++ src/main/kotlin/com/whatsappgroups/application/dto/MercadoLivreDtos.kt
@@ -1,10 +1,32 @@
 package com.whatsappgroups.application.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class MlStatusResponse(
     val connected: Boolean,
-    val nickname: String?
+    val nickname: String?,
+    val tokenValid: Boolean = false,
+    val tokenExpired: Boolean = false,
+    val affiliateConfigured: Boolean = false,
+    val error: String? = null
 )
 
 data class MlOAuthStartResponse(
     val authorizationUrl: String
+)
+
+data class MlAffiliateParamsRequest(
+    val mattWord: String,
+    val mattTool: String
+)
+
+data class MlItemDetails(
+    val id: String,
+    val title: String,
+    val permalink: String?,
+    val thumbnail: String?,
+    val price: Double?,
+    @JsonProperty("currency_id") val currencyId: String?,
+    val condition: String?,
+    @JsonProperty("available_quantity") val availableQuantity: Int?
 )
